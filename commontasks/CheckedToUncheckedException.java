d6245ac7696516a7eb6e8b4bbada50de10730e3a
Zemian Deng <zemiandeng@gmail.com>
2020-11-25 11:26:15 -0500

    Add exception handling tasks

---
+++ commontasks/CheckedToUncheckedException.java
@@ -0,0 +1,13 @@
+public class CheckedToUncheckedException {
+    public static void main(String[] args) {
+        sleep(1000); // It's optional to use 'L' suffix!
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Failed to sleep full " + millis + " millis seconds.", e);
+        }
+    }
+}
