f2fda0b3fd014a6d9cb7a5251b7b1f039e82f270
Zemian Deng <zemiandeng@gmail.com>
2020-12-11 19:03:06 -0500

    Add JavaScriptPlugin

---
+++ topics/minecraft/javascript-plugin/readme.md
@@ -0,0 +1,5 @@
+https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino
+
+https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino/Shell
+
+    java org.mozilla.javascript.tools.shell.Main [options] script-filename-or-url [script-arguments]
---
+++ topics/minecraft/javascript-plugin/src/main/java/zemian/minecraft/scriptingplugin/JavaScriptCommand.java
@@ -0,0 +1,44 @@
+package zemian.minecraft.scriptingplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JavaScriptCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] strings) {
+        Context cx = Context.enter();
+
+        String mcScript = System.getProperty("user.home") + "/mc.js";
+        if (strings.length > 0) {
+            mcScript = strings[0];
+        }
+        File file = new File(mcScript);
+        System.out.println("Executing JavaScript: " + file);
+        try {
+            Scriptable scope = cx.initStandardObjects();
+            ScriptableObject.putProperty(scope, "sender", Context.javaToJS(sender, scope));
+            ScriptableObject.putProperty(scope, "command", Context.javaToJS(command, scope));
+            ScriptableObject.putProperty(scope, "label", Context.javaToJS(label, scope));
+            ScriptableObject.putProperty(scope, "strings", Context.javaToJS(strings, scope));
+
+            try (FileReader reader = new FileReader(file)) {
+                Object result = cx.evaluateReader(scope, reader, mcScript, 1, null);
+                // Convert the result to a string and print it.
+                System.out.println(Context.toString(result));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to run JavaScript", e);
+        } finally {
+            cx.exit();
+        }
+        return false;
+    }
+}
