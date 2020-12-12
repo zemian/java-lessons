29f7a51d6c2c55225dd6226a94a3420503d9758b
Zemian Deng <zemiandeng@gmail.com>
2020-12-11 20:07:01 -0500

    Switch JavaScript engine to Nashhorn

--- topics/minecraft/javascript-plugin/readme.md
+++ topics/minecraft/javascript-plugin/readme.md
@@ -1,5 +1,18 @@
-https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino
+## JDK Nashhorn
 
-https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino/Shell
+It's built-in in JDK 8  to 14. Try running `jjs` command. (Nashhorn version 14)
+
+Starting JDK 15, it will have separate package from JDK. (Nashhorn version 15)
+
+* https://github.com/openjdk/nashorn
+* https://search.maven.org/artifact/org.openjdk.nashorn/nashorn-core/15.0/jar
+* https://mail.openjdk.java.net/pipermail/nashorn-dev/2020-October/007557.html
+
+
+## Mozilla Rhino Project
+
+* https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino
+* http://mozilla.github.io/rhino/javadoc/index.html
+* https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino/Shell
 
     java org.mozilla.javascript.tools.shell.Main [options] script-filename-or-url [script-arguments]
--- topics/minecraft/javascript-plugin/src/main/java/zemian/minecraft/javascriptplugin/JavaScriptCommand.java
+++ topics/minecraft/javascript-plugin/src/main/java/zemian/minecraft/javascriptplugin/JavaScriptCommand.java
@@ -3,13 +3,12 @@ package zemian.minecraft.javascriptplugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.ScriptableObject;
 
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class JavaScriptCommand implements CommandExecutor {
     @Override
@@ -21,24 +20,20 @@ public class JavaScriptCommand implements CommandExecutor {
         File file = new File(mcScript);
         System.out.println("Executing JavaScript: " + file);
 
-        Context cx = Context.enter();
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("nashorn");
         try {
-            Scriptable scope = cx.initStandardObjects();
-            ScriptableObject.putProperty(scope, "sender", Context.javaToJS(sender, scope));
-            ScriptableObject.putProperty(scope, "command", Context.javaToJS(command, scope));
-            ScriptableObject.putProperty(scope, "label", Context.javaToJS(label, scope));
-            ScriptableObject.putProperty(scope, "strings", Context.javaToJS(strings, scope));
+            Bindings bindings = engine.createBindings();
+            bindings.put("sender", sender);
+            bindings.put("command", command);
+            bindings.put("label", label);
+            bindings.put("strings", strings);
 
             try (FileReader reader = new FileReader(file)) {
-                cx.evaluateReader(scope, reader, mcScript, 1, null);
-                //Object result = cx.evaluateReader(scope, reader, mcScript, 1, null);
-                // Convert the result to a string and print it.
-                //System.out.println(Context.toString(result));
+                engine.eval(reader, bindings);
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException("Failed to run JavaScript", e);
-        } finally {
-            cx.exit();
         }
         return false;
     }
