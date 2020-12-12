e56ca1a594fa798e481d9d0b50f960b32fa87e9d
Zemian Deng <zemiandeng@gmail.com>
2020-12-12 17:54:18 -0500

    Add ScriptingTask

---
+++ commontasks/ScriptingTask.java
@@ -0,0 +1,28 @@
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.io.File;
+import java.io.FileReader;
+
+public class ScriptingTask {
+    public static void main(String[] args) throws Exception {
+        String script = (args.length > 0) ? args[0] : "commontasks/hello.js";
+        runScript(script);
+    }
+
+    public static void runScript(String script) {
+        try {
+            String ext = script.substring(script.lastIndexOf(".") + 1);
+            ScriptEngineManager factory = new ScriptEngineManager();
+            ScriptEngine engine = factory.getEngineByName(ext);
+            File file = new File(script);
+            Bindings bindings = engine.createBindings();
+            try (FileReader reader = new FileReader(file)) {
+                engine.eval(reader, bindings);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to run script " + script, e);
+        }
+    }
+
+}
