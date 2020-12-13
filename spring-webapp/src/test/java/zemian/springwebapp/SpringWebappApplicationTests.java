e3a08049a9a382598d0c0f1387ed3fb5e55563e3
Zemian Deng <zemiandeng@gmail.com>
2020-12-12 21:14:59 -0500

    Add spring-webapp

---
+++ spring-webapp/src/test/java/zemian/springwebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package zemian.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
